abe20f94c61a56929c47823d868592895c4869ca
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-08-06 17:58:52 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,35 @@
 class BankAccount
-
+  attr_accessor :balance, :status
+  attr_reader :name 
+  
+  def initalize(name)
+    @name = name 
+    @balance = 1000
+    @status = "open"
+    
 end
+
+def deposit(amount)
+  @balance += amount 
+end 
+
+def display_balance 
+  "Your Balnce is $#{@balance}."
+end 
+
+def vaild?
+  if @status == "open" && @balance > 0 
+    true 
+  else
+    false 
+    
+  end 
+  
+end 
+
+def close_account
+  @status = "cloud"
+  
+end 
+
+end 
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,41 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status, :count 
+
+def initialize (sender, receiver, amount)
+  @sender = sender 
+  @receiver = receiver
+  @amount = amount 
+  @statues = "pending"
+end
+
+def both_valid?
+  if sender.vaild? && receiver.vaild?
+    true 
+  else 
+    false 
+  end 
+end 
+
+def execute_transaction
+    if @sender.balance < @amount
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+
+    elsif @status == "complete"
+      puts "Transaction was already excuted"
+    else
+      @sender.deposit( @amount * -1 ) 
+      @receiver.deposit( @amount )
+      @status = "complete"
+    end
+  end
+
+  def reverse_transfer
+    if @status == "complete"
+      @sender.deposit( @amount ) 
+      @receiver.deposit( @amount * -1)
+      @status = "reversed"
+    end
+  end
+
 end
