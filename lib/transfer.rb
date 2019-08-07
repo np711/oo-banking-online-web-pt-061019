01578a06e56c039366fd2b8cbf1911cae8aa6302
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-08-07 03:09:08 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -11,27 +11,25 @@ class Transfer
   end
 
    def valid?
-    sender.valid? && receiver.valid?
+    sender.valid? && @receiver.valid? ? true : false 
   end
 
    def execute_transaction
-    if valid? && sender.balance > amount && self.status == "pending"
-      sender.withdrawal(self.amount)
-      receiver.deposit(self.amount)
-      self.status = "complete"
+    if @sender.balance > @amount && @status == "pending"
+      @sneding.balance -= @amount
+      @receiver.balance += @amount
+      @status = "complete"
     else
-      self.status = "rejected"
-      "Transaction rejected. Please check your account balance."
+      @status = "rejected"
+      return "Transaction reject. Please check your account balance."
     end
   end
 
    def reverse_transfer
-    if valid? && receiver.balance > amount && self.status == "complete"
-      sender.deposit(self.amount)
-      receiver.withdrawal(self.amount)
-      self.status = "reversed"
-    else
-      self.status = "rejected"
-      "Transaction rejected. Please check your account balance."            
-    end
-  end
+    if @status == "complete"
+      @sender.balance += @amount 
+      @receiver.balance -= @amount
+      @status = "reversed"
+    end 
+  end 
+end 
